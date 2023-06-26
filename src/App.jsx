8c16f791e5309e1e1ca1a50013811133e915a216
AvnerGold <avnergoldberg@hotmail.fr>
2023-06-26 17:26:18 +0300

    app start

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState('');
+
+  const API_KEY = ' b6a75494f9a85d418a7a87658086af69';
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setWeatherData(data);
+        setError('');
+      } else {
+        setWeatherData(null);
+        setError('Failed to fetch weather data.');
+      }
+    } catch (error) {
+      setWeatherData(null);
+      setError('Error occurred while fetching weather data.');
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeatherData();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div className="weather-card">
+      <h2>Weather App</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a city"
+          value={city}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData && (
+        <div className="weather-info">
+          <h3>{weatherData.name}</h3>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+      {error && <p className="error-message">{error}</p>}
+    </div>
     </>
   )
 }
